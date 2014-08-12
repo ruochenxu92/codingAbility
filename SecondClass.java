5147c10432c83e9cfe6b6dd97edfeaae0db57a32
xxu46 <xxu46@illinois.edu>
2014-08-11 20:16:40 -0500

    Second Chapter Binary Search and Sorted Array

--- SecondClass.java
+++ SecondClass.java
@@ -736,4 +736,41 @@ public class SecondClass {
 	 		}
 	 	}
 	 	
+	 	
+	 	/**
+	 	 * 11.1   3 Sum
+	 	 * sort array,
+	 	 * binary search and skip duplicate
+	 	 */
+	 	/*
+	 	    Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+			Note:
+			Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
+			The solution set must not contain duplicate triplets.
+			    For example, given array S = {-1 0 1 2 -1 -4},
+			
+			    A solution set is:
+			    (-1, 0, 1)
+			    (-1, -1, 2)
+		*/
+
+	 	
+	 	/**
+	 	 * 11.2   4 Sum
+	 	 * sort array
+	 	 * binary search and skip duplicate
+	 	 */
+	 	/*
+	 	    Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+			Note:
+			Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a ≤ b ≤ c ≤ d)
+			The solution set must not contain duplicate quadruplets.
+			    For example, given array S = {1 0 -1 0 -2 2}, and target = 0.
+			
+			    A solution set is:
+			    (-1,  0, 0, 1)
+			    (-2, -1, 1, 2)
+			    (-2,  0, 0, 2)
+	 	 */
 }
